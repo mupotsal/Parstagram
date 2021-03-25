a266e1855536f73d97aae3bde103fc05f2549d8c
liberty Mupotsa <mupotsal@berea.edu>
2021-03-25 18:53:35 -0400

    The Login, Stay Logged In is Done

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -94,5 +94,14 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
+    
 
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        let delegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        delegate.window?.rootViewController = loginViewController
+    }
 }
